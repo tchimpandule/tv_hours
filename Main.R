2930d1231a3f0ae36090846934995b93c1f7347d
Tiwonge Chimpandule <67604280+tchimpandule@users.noreply.github.com>
2024-04-16 21:28:51 +0200

    Changed the ae limit and selected whites

--- Main.R
+++ Main.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(here)
 Marital_tv_hous<-gss_cat %>% group_by(marital) %>% 
-  filter(age>40) %>% 
+  filter(age>70) %>% 
+  select(race="White")%>% 
   summarise(mean_tv_hours=mean(tvhours,na.rm=T))
 
 write.csv(Marital_tv_hous,here("Marital_mean_tv_hours.csv"))
