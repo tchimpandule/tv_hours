6870a3aac5935921ea9fb49fba88069ada6ba1aa
Tiwonge Chimpandule <67604280+tchimpandule@users.noreply.github.com>
2024-04-16 21:02:46 +0200

    Updated the age to more than 40

--- Main.R
+++ Main.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(here)
 Marital_tv_hous<-gss_cat %>% group_by(marital) %>% 
-  filter(age>30) %>% 
+  filter(age>40) %>% 
   summarise(mean_tv_hours=mean(tvhours,na.rm=T))
 
 write.csv(Marital_tv_hous,here("Marital_mean_tv_hours.csv"))
